22481372e7919b258d42f0c118e81f3bca139310
jwilliamson <j.williamson@gambler-labs.com>
2023-01-17 21:39:49 -0500

    honestly could be better

---
+++ src/InstanceBuilder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ComputeEngine;
+
+class InstanceBuilder{
+    private $deploymentImageLocation;
+    private $installLocation;
+
+    private $os;
+    private $version;
+
+    private $deploymentImageExtension = 'deployment-image.qcow2.zst';
+
+    private $sshSession;
+
+    public function __construct($deploymentLocation, $installLocation){
+        $this->deploymentImageLocation = $deploymentLocation;
+        $this->installLocation = $installLocation;
+
+        $this->sshSession = ssh2_connect('127.0.0.1', 22);
+        ssh2_auth_password($this->sshSession, $_ENV['SSHUSERNAME'], $_ENV['SSHPASSWORD']);
+    }
+
+    public function setOS($os): self{
+        $this->os = $os;
+
+        return $this;
+    }
+
+    public function setVersion($version): self{
+        $this->version = $version;
+
+        return $this;
+    }
+
+    public function downloadDeploymentImage(): self{
+        if(!file_exists("{$this->deploymentImageLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension}")){
+            throw new \Exception('Deployment image does not exist');
+        }
+
+        if(file_exists("{$this->installLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension}")){
+            return $this;
+        }
+
+        ssh2_exec($this->sshSession, "cp {$this->deploymentImageLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension} {$this->installLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension}");
+        
+        return $this;
+    }
+
+    public function unpackDeploymentImage(): self{
+        ssh2_exec($this->sshSession, "cd {$this->installLocation} && zstd -d -T0 {$this->installLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension}");
+
+        return $this;
+    }
+
+    public function moveToFinalName($name): self{
+        ssh2_exec($this->sshSession, "mv {$this->installLocation}/{$this->os}-{$this->version}-{$this->deploymentImageExtension} {$this->installLocation}/$name.qcow2");
+
+        return $this;
+    }
+
+    public function installServer($options): self{
+        ssh2_exec($this->sshSession, "virt-install --name {$options['name']} --memory {$options['memory']} --vcpus {$options['vcpus']} --disk {$options['disk']} --import --os-variant generic --noautoconsole");
+
+        return $this;
+    }
+
+}
---
+++ tests/InstanceBuilderTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace ComputeEngine\Test;
+
+use PHPUnit\Framework\TestCase;
+use Dotenv\Dotenv;
+
+use ComputeEngine\InstanceBuilder;
+
+class InstanceBuilderTest extends TestCase{
+
+    private $instanceBuilder;
+
+    public function setUp(): void{
+        $dotenv = Dotenv::createImmutable('/mnt/d/WebDevelopment/ComputeEngine/');
+        $dotenv->load();
+
+        $this->instanceBuilder = new InstanceBuilder('/mnt/d/WebDevelopment/ComputeEngine/private/deploymentImages', '/var/lib/libvirt/images');
+    }
+
+    public function tearDown(): void{
+        $session = ssh2_connect('127.0.0.1', 22);
+        ssh2_auth_password($session, $_ENV['SSHUSERNAME'], $_ENV['SSHPASSWORD']);
+
+        ssh2_exec($session, 'rm -f /var/lib/libvirt/images/*-deployment-*');
+        ssh2_exec($session, 'rm -f /var/lib/libvirt/images/testing.qcow2');
+        ssh2_exec($session, 'virsh destroy newServer && virsh undefine newServer');
+        ssh2_exec($session, 'rm -f /var/lib/libvirt/images/newServer.qcow2');
+    }
+
+    public function imageDataProvider(){
+        return [
+            ['centos', '7'],
+            ['debian', '11']
+        ];
+    }
+
+    /**
+     * @covers downloadDeploymentImage
+     * @dataProvider imageDataProvider
+     */
+    public function testDownloadDeploymentImage($os, $version){
+        $this->instanceBuilder->setOS($os)
+            ->setVersion($version);
+
+        $this->assertEquals(InstanceBuilder::class, $this->instanceBuilder->downloadDeploymentImage()::class);
+        $this->assertFileExists("/var/lib/libvirt/images/$os-$version-deployment-image.qcow2.zst");
+    }
+
+    /**
+     * @covers downloadDeploymentImage
+     */
+    public function testDownloadDeploymentImageThrowsErrorIfImageDoesNotExist(){
+        $this->expectErrorMessage('Deployment image does not exist');
+
+        $this->instanceBuilder->setOS('randomos')
+            ->setVersion('3erre')
+            ->downloadDeploymentImage();
+    }
+
+    /**
+     * @covers unpackDeploymentImage
+     * @dataProvider imageDataProvider
+     */
+    public function testUnpackDeploymentImage($os, $version){
+        $this->instanceBuilder->setOS($os)
+            ->setVersion($version)
+            ->downloadDeploymentImage();
+
+        $this->assertEquals(InstanceBuilder::class, $this->instanceBuilder->unpackDeploymentImage()::class);
+        $this->assertFileExists("/var/lib/libvirt/images/$os-$version-deployment-image.qcow2");
+    }
+
+    /**
+     * @covers moveToFinalName
+     */
+    public function testMoveToFinalName(){
+        $os = 'centos';
+        $version = '7';
+        $name = 'testing';
+
+        $this->instanceBuilder->setOS($os)
+            ->setVersion($version)
+            ->downloadDeploymentImage()
+            ->unpackDeploymentImage();
+
+        $this->assertEquals(InstanceBuilder::class, $this->instanceBuilder->moveToFinalName($name)::class);
+        $this->assertFileExists("/var/lib/libvirt/images/$name.qcow2");
+    }
+
+    /**
+     * @covers installServer
+     */
+    public function testInstallServer(){
+        $os = 'centos';
+        $version = '7';
+        $name = 'newServer';
+
+        $this->instanceBuilder->setOS($os)
+            ->setVersion($version)
+            ->downloadDeploymentImage()
+            ->unpackDeploymentImage()
+            ->moveToFinalName($name);
+
+        $options = [
+            'name' => $name,
+            'memory' => '2048',
+            'vcpus' => '2',
+            'disk' => "/var/lib/libvirt/images/$name.qcow2"
+        ];
+
+        $this->assertEquals(InstanceBuilder::class, $this->instanceBuilder->installServer($options)::class);
+        $this->assertFileExists("/var/lib/libvirt/images/$name.qcow2");
+    }
+}
