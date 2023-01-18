f43973447775d596096d8a4c8371f8a011a5f33a
jwilliamson <j.williamson@gambler-labs.com>
2023-01-17 21:39:22 -0500

    a test to make sure the test works

---
+++ tests/ClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ComputeEngine\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase{
+
+    /**
+     * @covers
+     */
+    public function testTestingWorks(){
+        $this->assertEquals(true, true);
+    }
+
+}
